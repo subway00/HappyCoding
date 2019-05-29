6aaa6a75009721f6b22751165aa3c8452783791a
subway00 <pig945513@gmail.com>
2019-05-29 23:22:00 +0800

    NEXT:   read interface. & implements. &包裹器  &args

--- openhome.cc/src/Exception/Average.java
+++ openhome.cc/src/Exception/Average.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Average {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         Scanner console = new Scanner(System.in);
         double sum=0;
         int count=0;
-        try {
-        int ch = System.in.read();  //read()會拋出IOException.
+        //try {
+        //int ch = System.in.read();  //read()會拋出IOException.
         while (true) {
             int number = console.nextInt();
             if (number == 0) {
@@ -18,7 +18,7 @@ public class Average {
             sum += number; //variable 使用前須先設值給他
             count ++;
         }
-        }  catch (IOException ex) {       }
+        //}  catch (IOException ex) {       }
         System.out.printf("平均 %.2f%n", sum / count);
     }
 }
