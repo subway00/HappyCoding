b4d87bcfa36d6ceb42276ab6745ede66eebbb4cd
subway <pig945513@gmail.com>
2019-05-29 16:53:12 +0800

    On course!

---
+++ openhome.cc/src/Exception/Average.java
@@ -0,0 +1,24 @@
+package Exception;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double sum=0;
+        int count=0;
+        try {
+        int ch = System.in.read();  //read()會拋出IOException.
+        while (true) {
+            int number = console.nextInt();
+            if (number == 0) {
+                break;
+            }
+            sum += number; //variable 使用前須先設值給他
+            count ++;
+        }
+        }  catch (IOException ex) {       }
+        System.out.printf("平均 %.2f%n", sum / count);
+    }
+}
