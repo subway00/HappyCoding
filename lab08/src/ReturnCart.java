2240fa09a39ec27544e1939ef0e5bb4baa0e0f8f
subway00 <pig945513@gmail.com>
2019-06-05 00:32:22 +0800

    NEXT:   read包裹器。  NOTE: interface 表擁有行為的關係，extends表是一種的關係。

--- lab08/src/ReturnCart.java
+++ lab08/src/ReturnCart.java
@@ -2,28 +2,30 @@
 import java.util.ArrayList;
 
 public class ReturnCart {
+
     private ArrayList<Returnable> a;
     private int total;
-    
+
     public ReturnCart() {
         a = new ArrayList<Returnable>();
         total = 0;
     }
+
     public void add(Returnable x) {        //polymorphism
         //total += x.getPrice();
         a.add(x);
     }
+
     public void displayInf() {
         for (Returnable x : a) {           //polymorphism
             if (x instanceof Clothes) {         //instanceof判斷物件形態 , x物件是否為Clothes type.
-                ((Clothes)x).displayInf();
+                ((Clothes) x).displayInf();
+            } else if (x instanceof Pants) {
+                ((Pants) x).displayInf();
             }
-            else if (x instanceof Pants) {
-            (Pants)x.displayInf();        
-        }
             //((Clothes)x).displayInf();      //原x 'Returnable' type,把x cast 'Clothes' type
             System.out.println(x.returnInf());
         }
-        
+
     }
 }
