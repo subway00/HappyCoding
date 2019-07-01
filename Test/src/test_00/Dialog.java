df8154b69a427aec05ae47dd370f1c99f7a3076a
subway00 <pig945513@gmail.com>
2019-07-01 20:58:15 +0800

    no message

---
+++ Test/src/test_00/Dialog.java
@@ -0,0 +1,21 @@
+package test_00;
+
+public class Dialog {
+
+ 
+
+    public Dialog setTitle(String title) {
+        return this;
+        
+    }
+
+    public Dialog setMessage(String message) {
+        return this;
+        
+    }
+
+    public Dialog setPositiveButton() {
+        return this;
+        
+    }
+}
---
+++ Test/src/test_00/DialogTest.java
@@ -0,0 +1,9 @@
+package test_00;
+public class DialogTest {
+    public static void main(String[] args) {
+        Dialog di = new Dialog();
+        di.setMessage("123").setTitle("qqq").setPositiveButton();
+        
+        System.out.println(di);
+    }
+}
